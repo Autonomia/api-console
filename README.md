2cc5a1345ac73f28c7f6b5a08752c63d1fbafa94
Marco Graziano <marco@visiblenergy.com>
2019-03-31 14:33:26 -0700

    use autonomia-api module

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # api-console
 Autonomia API console application
+
+```
+export API_KEY=[APPLICATION_KEY] 
+export API_SECRET=[APPLICATION_SECRET]
+```
+
+```
+$ npm install
+$ npm run dev
+```
--- src/Devices.js
+++ src/Devices.js
@@ -1,17 +1,28 @@
 import React from "react";
+import ap from "autonomia-api";
+
+const autonomia = ap({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Devices extends React.Component {
-	state = {
-      	devices: []
-	};
+  state = {
+    vehicles: []
+  };
 
-	render() {
-	    return (
-	      <div>
-	      	DEVICES
-	      </div>
-	    );
-	}
+  componentDidMount() {
+    autonomia.vehicles
+      .list()
+      .then (data => {
+        console.log(data);
+        this.setState({ vehicles: data });
+      })
+  }
+
+  render() {
+    return <div>VEHICLES</div>
+  }
 }
 
 export default Devices;
