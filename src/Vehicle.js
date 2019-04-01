dd4456e58b2c2ba4135acb3336617ca9973a4ad7
Marco Graziano <marco@visiblenergy.com>
2019-04-01 15:40:29 -0700

    initial commit

---
+++ src/Vehicle.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+class Vehicle extends React.Component {
+  render() {
+    const { serial, category, camera, id } = this.props;
+    let hero = camera.urlImage;
+
+    return (
+      <Link to={`/details/${id}`} className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{serial}</h1>
+          <h2>{`${category} — ${camera.name} — ${camera.streaming}`}</h2>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default Vehicle;
